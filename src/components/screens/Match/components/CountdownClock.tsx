af1fe1a083cff515c0004be7945191c011150054
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-03-06 12:56:44 -0800

    :bug: clockdown clock resets appropriately

--- src/components/screens/Match/components/CountdownClock.tsx
+++ src/components/screens/Match/components/CountdownClock.tsx
@@ -9,24 +9,42 @@ const CountdownClock = (props: Props) => {
   const [currentTime, setCurrentTime] = useState(0);
   const prevStartTime = useRef<number>(0);
   const prevTime = useRef<number>(0);
+  const isPlaying = useRef(false);
   const requestRef = useRef<
     ReturnType<typeof requestAnimationFrame> | undefined
   >();
 
+  console.log("[CLOCK] currentTime", currentTime);
+
   const animate = (time: number) => {
+    console.log(
+      "[CLOCK] Tick tock.",
+      prevTime.current,
+      time,
+      prevStartTime.current
+    );
+
+    // No previous time? Assume it's the current time.
+    // When we reset game, the RAF time keeps going, so zeroing breaks time
+    if (prevTime.current === 0) prevTime.current = time;
+
+    // Get the latest state (since the callback restricts reaching out)
     const { mode } = useGameStore.getState();
     // Repeat
-    if (mode === "MATCH") requestRef.current = requestAnimationFrame(animate);
+    if (mode === "MATCH" && isPlaying.current)
+      requestRef.current = requestAnimationFrame(animate);
 
     // Set time. We use time value from RAF.
     const deltaTime = time - prevTime.current;
+    console.log("[CLOCK] Delta time", deltaTime * 0.01);
     setCurrentTime((state) => state + deltaTime * 0.01);
     prevTime.current = time;
   };
 
   // Start the clock (unless it's game over screen)
   useEffect(() => {
-    if (mode === "MATCH") requestRef.current = requestAnimationFrame(animate);
+    if (mode === "MATCH" && isPlaying.current)
+      requestRef.current = requestAnimationFrame(animate);
     return () => {
       if (requestRef.current) cancelAnimationFrame(requestRef.current);
     };
@@ -34,17 +52,29 @@ const CountdownClock = (props: Props) => {
 
   // Trigger "Game Over" screen when time has run out
   useEffect(() => {
-    if (currentTime > MATCH_LENGTH_TIME) {
+    if (currentTime > MATCH_LENGTH_TIME && isPlaying.current) {
       setMode("GAME-OVER");
-      console.log("game over");
+      console.log("game over", currentTime);
+      isPlaying.current = false;
     }
   }, [currentTime]);
 
+  // // Trigger "Game Over" screen when time has run out
+  // useEffect(() => {
+  //   if (mode === "MATCH") {
+  //     console.log("[CLOCK] Resetting clock");
+  //     isPlaying.current = true;
+  //     setCurrentTime(0);
+  //   }
+  // }, [mode]);
+
   // Reset the clock if new game
   useEffect(() => {
-    if (startTime !== prevStartTime.current) {
+    if (startTime !== prevStartTime.current && !isPlaying.current) {
+      console.log("[CLOCK] Resetting clock");
       prevStartTime.current = startTime;
       setCurrentTime(0);
+      isPlaying.current = true;
     }
   }, [startTime]);
 
